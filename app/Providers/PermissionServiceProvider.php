8825cb375192ead1f88ecc5c3e337c9cc6dcbb92
brickz28 <brickz28@comcast.net>
2018-12-02 16:53:16 -0600

    perms service provider added

---
+++ app/Providers/PermissionServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+use App\Permission;
+use Gate;
+
+class PermissionServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Permission::get()->map(function ($permission){
+            Gate::define($permission->name,function($user) use ($permission)
+            {
+                return $user->hasPermission($permission);
+            });
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
