25b5e1729d0413009116d1f02702b50bafbb82fb
brickz28 <brickz28@comcast.net>
2018-12-15 21:21:34 -0600

    password restting usuability

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'admin@arkafterearthps4.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
