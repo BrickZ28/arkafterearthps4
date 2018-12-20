732f1ef9da89c4e81dc498b8c08b2516e7ff9a5c
brickz28 <brickz28@comcast.net>
2018-12-19 19:10:50 -0600

    add perm controller

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Permission;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $perms = Permission::all();
+
+        return view('ark.perms')->with('perms', $perms);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('ark.addPerm');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $attributes = request()->validate([
+            'name'  => 'required',
+            ]);
+
+        Permission::create($attributes);
+
+        return redirect('/permissions');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Permission $permission)
+    {
+
+        return view('ark.editPerms', compact('permission'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Permission $permission)
+    {
+        $permission->update([
+            'name' => \request('name')
+        ]);
+
+        return redirect('/permissions')->with('success', $permission->name . ' updated');
+    }
+
+    public function destroy(Permission $permission)
+    {
+        $permission->delete();
+
+        return redirect('/permissions')->with('success', $permission->name . ' permission deleted');
+    }
+}
