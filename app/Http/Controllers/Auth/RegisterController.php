1310d4598d8def1f276681337033bc35ad7bb0af
brickz28 <brickz28@comcast.net>
2018-12-21 09:58:46 -0600

    default role and permisisons

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Role;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
@@ -64,11 +65,21 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        if(empty($data['tribename'])){
+            $data['tribename'] = 'None';
+        }
+
+         $user = User::create([
             'name' => $data['name'],
             'tribename' => $data['tribename'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+       $user->roles()->attach('4');
+       $user->permissions()->attach('8');
+
+       return $user;
+
     }
 }
