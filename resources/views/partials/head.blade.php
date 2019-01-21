650d6b41b811d3d69726c93a5e1a6fe6b681dd65
brickz28 <brickz28@comcast.net>
2019-01-21 09:40:10 -0600

    Add meta tags

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,8 +3,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="keywords" content="Ark, Afterearth, PS4, Gaming, Multiplayer, Extinction, Ragnorok, The Island, Discord, Free, Active, Playstation, Playstation 4, Server, Cluster,">
+    <meta name="description" content="Ark PS4 Multiplayer Server that is clustered">
+    <meta name="author" content="BrickSyS">
     <link rel="icon" href=" ">
     <title>Afterearth Ark Server</title>
 
