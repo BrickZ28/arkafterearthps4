f7f51938f41828b10eb43d93a6844939c55cf0fc
brickz28 <brickz28@comcast.net>
2019-01-20 22:36:26 -0600

    removed store link and added link to FB page

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,6 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-6 col-md-3"> &copy;<script type="text/javascript">document.write(new Date().getFullYear());</script> arkafterearthps4.com </div>
+            <div class="col-sm-6 col-md-3 pull-right text-xs-right"<li> <a href="https://www.facebook.com/ArkAfterearth/"><i class="fa fa-facebook-square" aria-hidden="true"></i>
+                </a></li> </div>
             <div class="col-sm-6 col-md-3 pull-right text-xs-right">A BrickSyS Website </div>
         </div>
     </div>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,10 +2,11 @@
     <div class="container relative-box "> <a class="navbar-brand" href="#">Afterearth</a>
         <button class="navbar-toggler hidden-lg-up collapsed" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2" aria-controls="exCollapsingNavbar2" aria-expanded="false" aria-label="Toggle navigation"> ☰ </button>
         <div class="navbar-toggleable-md collapse" id="exCollapsingNavbar2" >
+
             <ul class="nav navbar-nav pull-xs-right">
                 <li class="nav-item active"> <a class="nav-link" href="#myCarousel">Home <span class="sr-only">(current)</span></a> </li>
                 <li class="nav-item"> <a class="nav-link" href="#rules">PVP Rules</a> </li>
-                <li class="nav-item"> <a class="nav-link" href="#">Council Store</a> </li>
+
                 {{--<li class="nav-item"> <a class="nav-link" href="/tribespvp">PVP Tribes</a> </li>
                 <li class="nav-item"> <a class="nav-link" href="#">PVE Tribes</a> </li>--}}
                 @if(Auth::check())
@@ -14,6 +15,8 @@
                     <li class="nav-item"> <a class="nav-link" href="/register">Register</a> </li>
                     <li class="nav-item"> <a class="nav-link" href="/login">Login</a> </li>
                 @endif
+                <li class="nav-item"> <a href="https://www.facebook.com/ArkAfterearth/"<i class="fa fa-facebook-square" aria-hidden="true"></i>
+                    </a></li>
             </ul>
         </div>
     </div>
