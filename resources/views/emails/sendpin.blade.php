be4d3c9407f1a6e4a9a91122fd0f2106a3f104e6
brick <brickz28@comcast.net>
2019-01-24 21:03:02 -0600

    add imag in pin

--- resources/views/emails/sendpin.blade.php
+++ resources/views/emails/sendpin.blade.php
@@ -5,18 +5,25 @@
     Pin:  {{$pin}}
     Gate:  {{$gate}}
 
+
+
+    @if($style === 'pve')
     To claim this, you can travel to the Community Center (CC) on Ragnarok by going by the Blue Obelisk
     and use the teleporter to go to the CC.
 
-    @if($style === 'pve')
     Pve Cords 28,49:
  ![See attached]({{asset('/img/pvePikUp.jpg')}})
     @endif
     @if($style === 'pvp')
-        PvP: Viking Bay 2
+     To claim this, you can travel to the Community Center (CC) on Ragnarok
+
+     PvP: Viking Bay 2
  ![See attached]({{asset('/img/pvpPikUp.jpg')}})
     @endif
 
+
+***
+ **If you cannot see an image, view in html**
 
     Thanks,
     {{ config('app.name') }}
