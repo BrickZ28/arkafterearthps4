e1b2561b667b7fcf1f68ed47a268a5792a073cd9
brick <brickz28@comcast.net>
2019-01-27 13:40:00 -0600

    bank 404 fixing

--- resources/views/bank/bank.blade.php
+++ resources/views/bank/bank.blade.php
@@ -54,10 +54,10 @@
     <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
-                <strong>Edit Dino Form</strong>
+                <strong>Add transaction</strong>
             </div>
             <div class="card-body card-block">
-                <form action="/bank/{{$bank->id}}"  method="post" enctype="multipart/form-data" class="form-horizontal">
+                <form action="/bank/{{$banks->id}}"  method="post" enctype="multipart/form-data" class="form-horizontal">
                     @method('PATCH')
                     @csrf
                     <div class="row form-group">
