8ba543e39aba2c27b2e4091616b371f5ff52af68
brick <brickz28@comcast.net>
2019-01-29 23:20:34 -0600

    show transactions where dion removed

--- resources/views/ark/manageMyFunds.blade.php
+++ resources/views/ark/manageMyFunds.blade.php
@@ -30,7 +30,7 @@
                     <tr>
                         <th scope="col">My Balance</th>
                         <th scope="col">Current Interest Rate</th>
-                        <th scope="col">Current Interest Earned <span class="badge badge-info">(paid weekly)</span></th>
+                        <th scope="col">Projected Interest Earned <span class="badge badge-info">(Fridays @ 12:00a.m. EST)</span></th>
                     </tr>
                     </thead>
                     <tbody>
