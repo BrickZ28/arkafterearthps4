784d03fc482fa226a003b4858aa45744da1eeb63
brick <brickz28@comcast.net>
2019-01-30 21:05:36 -0600

    app user not found

--- app/Http/Controllers/GateController.php
+++ app/Http/Controllers/GateController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Gate;
 use App\Rules\SameGateStyle;
-use App\User;
+
 use Illuminate\Http\Request;
 use function PhpParser\filesInDir;
 use Illuminate\Support\Facades\Auth;
