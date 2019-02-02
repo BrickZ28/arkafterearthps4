6155ff90cd16da53d74ea6d0d75eb032b1bc5182
brick <brickz28@comcast.net>
2019-02-02 12:19:50 -0600

    edit out search to fix it

--- resources/views/ark/inTransactions.blade.php
+++ resources/views/ark/inTransactions.blade.php
@@ -16,13 +16,12 @@
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                @endif{{--
-                <form action="/searchTransactionsByUser" method="get">
+                @endif
+                {{--<form action="/searchTransactionsByUser" method="get">
                     @csrf
                     <input  name="search_text" placeholder="Insert value to search" class="text-muted" type="text"/>
                     <button type="submit" class="btn btn-primary">Search Transactions</button>
-                </form>
---}}
+                </form>--}}
                 <table class="table table-striped">
                     <thead>
                     <tr>
@@ -70,11 +69,11 @@
                         </button>
                     </div>
                 @endif
-                {{--<form action="/searchTransactionsFromBank" method="get">
+                <form action="/searchTransactionsFromBank" method="get">
                     @csrf
                     <input  name="search_text" placeholder="Insert value to search" class="text-muted" type="text"/>
                     <button type="submit" class="btn btn-primary">Search Transactions</button>
-                </form>--}}
+                </form>
 
                 <table class="table table-striped">
                     <thead>
