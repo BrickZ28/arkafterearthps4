ac9650b58d5239a6b02984eb7581f0e4f0c269bd
brick <brickz28@comcast.net>
2019-02-02 12:45:03 -0600

    edit out search to fix it

--- resources/views/ark/outTransactions.blade.php
+++ resources/views/ark/outTransactions.blade.php
@@ -17,11 +17,11 @@
                         </button>
                     </div>
                 @endif
-                <form action="/searchTransactionsPyUser" method="get">
+               {{-- <form action="/searchTransactionsPyUser" method="get">
                     @csrf
                     <input  name="search_text" placeholder="Insert value to search" class="text-muted" type="text"/>
                     <button type="submit" class="btn btn-primary">Search Transactions</button>
-                </form>
+                </form>--}}
 
                 <table class="table table-striped">
                     <thead>
