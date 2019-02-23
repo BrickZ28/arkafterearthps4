8137cf442e125086e44c25a97fd0a2203437b786
Brick <brickz28@comcast.net>
2019-02-23 15:14:16 -0600

    dino imgaes

--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -82,7 +82,7 @@
                     <tbody>
                     @foreach($dinos as $dino)
                         <tr>
-                            <td>{{$dino->name}} <span><img style="width: 80%; max-width: 300px;" src="{{$dino->img}}"></span></td>
+                            <td>{{$dino->name}} <span><img style="width: 90%; max-width: 300px;" src="{{$dino->img}}"></span></td>
                             <td>{{$dino->price}}</td>
                             <td>{{$dino->qty}}</td>
                             <td>{{$dino->level}}</td>
