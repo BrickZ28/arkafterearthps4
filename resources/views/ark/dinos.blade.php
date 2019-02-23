b9c1e88a82ca02ee169d48cd326b3697dde67ff3
Brick <brickz28@comcast.net>
2019-02-23 15:58:17 -0600

    dino imgaes

--- resources/views/ark/dinos.blade.php
+++ resources/views/ark/dinos.blade.php
@@ -64,46 +64,48 @@
                         </form>
                     @endif
                 @endif
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th scope="col">Dino Name</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">In Stock</th>
-                        <th scope="col">Estimated Level</th>
-                        <th scope="col">Dino Platform</th>
-                        <th scope="col">Dino Details</th>
-                        <th scope="col">Request Dino</th>
-                        @can('PVP Dino Seller')
-                        <th scope="col">Update Dino</th>
-                            @endcan
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($dinos as $dino)
+                    <table class="table table-striped"  >
+                        <thead>
                         <tr>
-                            <td>{{$dino->name}} <span class="photo media-left"><img  src="{{$dino->img}}"></span></td>
-                            <td>{{$dino->price}}</td>
-                            <td>{{$dino->qty}}</td>
-                            <td>{{$dino->level}}</td>
-                            <td>{{$dino->platform}}</td>
-                            <td>{{$dino->details}}</td>
-                            <td>
-                                <a href="/dinos/requestDino/{{$dino->id}}">
-                                    <button type="button" class="btn btn-secondary btn-sm" >Request Dino</button>
-                                </a>
-                            </td>
+                            <th scope="col">Dino Name</th>
+                            <th scope="col"></th>
+                            <th scope="col">Price</th>
+                            <th scope="col">In Stock</th>
+                            <th scope="col">Estimated Level</th>
+                            <th scope="col">Dino Platform</th>
+                            <th scope="col">Dino Details</th>
+                            <th scope="col">Request Dino</th>
                             @can('PVP Dino Seller')
-                            <td>
-                                <a href="/dinos/{{$dino->id}}">
-                                    <button type="button" class="btn btn-secondary btn-sm">Update Dino</button>
-                                </a>
-                            </td>
-                                @endcan
+                                <th scope="col">Update Dino</th>
+                            @endcan
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach($dinos as $dino)
+                            <tr>
+                                <td>{{$dino->name}}</td>
+                                <td><span><img src="{{$dino->img}}"></span></td>
+                                <td>{{$dino->price}}</td>
+                                <td>{{$dino->qty}}</td>
+                                <td>{{$dino->level}}</td>
+                                <td>{{$dino->platform}}</td>
+                                <td>{{$dino->details}}</td>
+                                <td>
+                                    <a href="/dinos/requestDino/{{$dino->id}}">
+                                        <button type="button" class="btn btn-secondary btn-sm" >Request Dino</button>
+                                    </a>
+                                </td>
+                                @can('PVP Dino Seller')
+                                    <td>
+                                        <a href="/dinos/{{$dino->id}}">
+                                            <button type="button" class="btn btn-secondary btn-sm">Update Dino</button>
+                                        </a>
+                                    </td>
+                                @endcan
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                     {{ $dinos->appends(request()->query())->links() }}
             </div>
 
