de99c7f692c5eafcd7c0fb844165654f097f4ec2
Brick <brickz28@comcast.net>
2019-02-23 14:27:35 -0600

    dino imgaes

--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -82,7 +82,7 @@
                     <tbody>
                     @foreach($dinos as $dino)
                         <tr>
-                            <td>{{$dino->name}} <span class="photo media-left"><img alt="t-rex" src="{{asset('https://arkafterearthbackup.sfo2.digitaloceanspaces.com/rexsm.png')}}"></span></td>
+                            <td>{{$dino->name}} <span class="photo media-left"><img  src="{{$dino->img}}"></span></td>
                             <td>{{$dino->price}}</td>
                             <td>{{$dino->qty}}</td>
                             <td>{{$dino->level}}</td>
