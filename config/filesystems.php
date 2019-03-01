959c506d61b63f42d05607baddd066c760ae38a4
BrickZ28 <brickz28@comcast.net>
2019-03-01 08:59:29 -0600

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,8 +57,8 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AKIAJUSBIGYOBPSMG25Q'),
-            'secret' => env('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'),
+            'key' => '',
+            
             'region' => env('US East (Ohio)'),
             'bucket' => env('afterearth'),
             'url' => env(' http://afterearth.s3.amazonaws.com'),
