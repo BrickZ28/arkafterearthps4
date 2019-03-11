cc25057fe5313b3c91358424104e34dd08e6ff68
Brick <brickz28@comcast.net>
2019-03-11 17:40:42 -0500

    add settings and img

--- resources/views/ark/settings.blade.php
+++ resources/views/ark/settings.blade.php
@@ -1,1 +1,32 @@
-<img src="{{asset('/img/afterearth_ad_1.png')}}" alt="">
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+        body {
+            background-color: #000000;
+        }
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        .imgbox {
+            display: grid;
+            height: 100%;
+        }
+        .center-fit {
+            max-width: 100%;
+            max-height: 100vh;
+            margin: auto;
+        }
+    </style>
+</head>
+<body>
+</body>
+</html>
+
+
+<div class="imgbox">
+    <img class="center-fit" src='{{asset('img/afterearth_ad_1.png')}}'>
+</div>
