ea9c78555477bcf328f71fb288ae2585ef03188a
Brick <brickz28@comcast.net>
2019-03-13 17:00:17 -0500

    Authenticate user system

--- resources/views/emails/sendwelcome.blade.php
+++ resources/views/emails/sendwelcome.blade.php
@@ -4,8 +4,8 @@
 Thank you for verifying your email and of course joining our server.  We hope you enjoy your time here as we try to give a balanced and fun experience.
 
 Before we can get you your starter kit and ability to use Discord to its fullest please follow the below steps
-1.  If you havnt done so already register on discord, you can use this link https://discord.gg/RnUXfdy
-2. Once in discord you will be greeted a welcome message asking you to select your play style PVP or PVE in the #welcome channel.  In the #welcome channel select your play style using the emojis
+1.  If you haven't done so already register on discord, you can use this link https://discord.gg/RnUXfdy
+2. Once in discord you will be put in the welcome channel asking you to select your play style, PVP or PVE in the #welcome channel.  In the #welcome channel select your play style using the emojis.
 ![See attached]({{asset('/img/profile/style.png')}})
 3. Now go to the #code channel and insert your registration code: **{{$code}}**
 4.  Now just browse around discord and read the channels.  Once an admin verifies your code you will receive another email with instructions on how to get your starter kit.
