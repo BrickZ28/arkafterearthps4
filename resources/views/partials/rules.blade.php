65ac31ac23806c4d2a474e23b34fd0f905b4e507
Brick <brickz28@comcast.net>
2019-03-15 14:20:48 -0500

    update pheonix owner

--- resources/views/partials/rules.blade.php
+++ resources/views/partials/rules.blade.php
@@ -33,7 +33,7 @@
             </div>
             <div class="col-md-4">
                 <div class="plan-block">
-                    <div class="heading"> <span class="plan-type">Pheonix</span> <span class="price">Redemption</span> <span class="duration">Current Owner</span> </div>
+                    <div class="heading"> <span class="plan-type">Redemption</span> <span class="price">Redemption</span> <span class="duration">Current Owner</span> </div>
                     <div class="detail-sec">
                         <ul>
                             <li>Any Tribe can callout Phoenix owner</li>
