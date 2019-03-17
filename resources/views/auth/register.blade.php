fe4eac50d6292b3dab12621f60308394eb45e19c
Brick <brickz28@comcast.net>
2019-03-16 23:40:29 -0500

    Store controller item controller create store page and store migration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
 
 
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -106,4 +106,4 @@
         </div>
     </div>
 </div>
-
+@endsection
