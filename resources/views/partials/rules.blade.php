940f4f259f43530afa83ef7e144924bd4fb993ee
Brick <brickz28@comcast.net>
2019-03-16 22:55:58 -0500

    Store controller item controller create store page and store migration

--- resources/views/partials/rules.blade.php
+++ resources/views/partials/rules.blade.php
@@ -6,8 +6,8 @@
             <div class="col-md-4">
                 <div class="plan-block">
                     <div class="heading"> <span class="plan-type">Pheonix</span> <span class="price">
-                            <img src="{{asset('img/dinos/pheonix.gif')}}" alt="" height="75" width="75">
-                            Redemption</span> <span class="duration">Current Owner</span> </div>
+                            <img src="{{asset('img/dinos/pheonix.gif')}}" alt="" height="50" width="50">
+                            Redemption<img src="{{asset('img/dinos/pheonix.gif')}}" alt="" height="50" width="50"></span> <span class="duration">Current Owner</span> </div>
                     <div class="detail-sec">
                         <ul>
                             <li>If Phoenix killed(travel or raid), show proof(pic)</li>
