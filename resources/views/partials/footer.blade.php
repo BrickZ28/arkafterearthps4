1e00cea30552f2f965e15adfa4f9e78a3045f889
Brick <brickz28@comcast.net>
2019-03-20 17:14:11 -0500

    added legal stuff to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,7 +4,13 @@
             <div class="col-sm-6 col-md-3"> &copy;<script type="text/javascript">document.write(new Date().getFullYear());</script> arkafterearthps4.com </div>
             <div class="col-sm-6 col-md-3 pull-right text-xs-right"<li> <a href="https://www.facebook.com/ArkAfterearth/"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
                 </a></li> </div>
-            <div class="col-sm-6 col-md-3 pull-right text-xs-right">A BrickSyS Website </div>
+        <div class="col-sm-6 col-md-2"><a href="https://disclaimergenerator.net/live.php?token=jLOxK5kmMqFz9g6zv3QjSsGk24rHov6Y">Disclaimer</a></div>
+
+        <div class="col-sm-6 col-md-2"><a href="https://disclaimergenerator.net/live.php?token=jLOxK5kmMqFz9g6zv3QjSsGk24rHov6Y">Privacy Policy</a></div>
+
+        <div class="col-sm-6 col-md-2"><a href="https://termsandcondiitionssample.com/live.php?token=Ngbe0o5kevDEWjduWbzkA97kScRKNUQQ">Terms and Conditions</a></div>
+
+            <div class="col-sm-6 col-md-2 pull-right text-xs-right">A BrickSyS Website </div>
         </div>
     </div>
 </footer>
