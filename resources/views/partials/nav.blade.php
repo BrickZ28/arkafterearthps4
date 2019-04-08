d8dd95c7f83cd60b9057de78f6c3b4ee889fea67
Brick <brickz28@comcast.net>
2019-04-08 15:19:20 -0500

    removed erroneous checkboxes

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -4,6 +4,7 @@
         <div class="navbar-toggleable-md collapse" id="exCollapsingNavbar2" >
 
             <ul class="nav navbar-nav pull-xs-right">
+
                 <li class="nav-item active"> <a class="nav-link" href="#myCarousel">Home <span class="sr-only">(current)</span></a> </li>
                 <li class="nav-item"> <a class="nav-link" href="#rules">PVP Rules</a> </li>
                 <li class="nav-item"> <a class="nav-link" href="/settings">Settings</a> </li>
@@ -15,7 +16,9 @@
                     <li class="nav-item"> <a class="nav-link" href="/register">Register</a> </li>
                     <li class="nav-item"> <a class="nav-link" href="/login">Login</a> </li>
                 @endif
-                <li class="nav-item"> <a href="https://www.facebook.com/ArkAfterearth/"<i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
+                <li class="nav-item"> <a href="https://www.facebook.com/ArkAfterearth/"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
+                    </a></li>
+                <li class="nav-item"> <a href="https://paypal.me/AfterEarthClusters?locale.x=en_US"><i class="fas fa-donate"></i>
                     </a></li>
             </ul>
         </div>
