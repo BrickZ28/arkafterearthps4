f0127c0353c8a739bf726eaf89738bc2243a1cec
Brick <brickz28@comcast.net>
2019-04-19 21:04:01 -0500

    update rues

--- resources/views/partials/rules.blade.php
+++ resources/views/partials/rules.blade.php
@@ -6,8 +6,8 @@
             <div class="col-md-4">
                 <div class="plan-block">
                     <div class="heading"> <span class="plan-type">Pheonix</span> <span class="price">
-                            <img src="{{asset('img/dinos/pheonix.gif')}}" alt="" height="50" width="50">
-                            Space Cowbells<img src="{{asset('img/dinos/pheonix.gif')}}" alt="" height="50" width="50"></span> <span class="duration">Current Owner</span> </div>
+                            <img src="{{asset('img/dinos/pheonix.gif')}}" alt="" height="40" width="40">
+                            Space Cowbells<img src="{{asset('img/dinos/pheonix.gif')}}" alt="" height="40" width="40"></span> <span class="duration">Current Owner</span> </div>
                     <div class="detail-sec">
                         <ul>
                             <li>If Phoenix gets killed in travelling or in a raid, show proof of assassination (pic).</li>
